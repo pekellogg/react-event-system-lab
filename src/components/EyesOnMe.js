d125d568eb964ca879087d41c7c4f2414b03b63c
Peyton Kellogg <pekellogg@hotmail.com>
2022-09-30 00:45:09 -0700

    commit tree b5db1ec7d485583296d28260995d8972f2fe94fb

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+
+  handleInputPassword = () => console.log("Entering password...")
+  
+  onFocus = () => console.log("Good!");
+
+  onBlur = () => console.log("Hey! Eyes on me!");
+
+  render() {
+    return (
+      <button onFocus={ this.onFocus } onBlur={ this.onBlur }>
+        Eyes on me, please!
+      </button>
+    )
+  }
+
+}
